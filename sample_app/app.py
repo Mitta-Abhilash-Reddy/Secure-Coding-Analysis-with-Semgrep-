9044689213966a2fe935e7deb62820e5d2cea16a
abhilash reddy <abhilashreddymitta@gmail.com>
2025-09-01 01:10:24 +0530

    version 2.1

--- sample_app/app.py
+++ sample_app/app.py
@@ -10,6 +10,8 @@ def ping():
     host = request.args.get('host', '127.0.0.1')
     # BAD: shell=True enables command injection
     output = subprocess.check_output(f"ping -c 1 {host}", shell=True)    
+    #output = subprocess.check_output(["ping", "-c", "1", host])
+
     return output
 
 @app.route('/search')
